ea582b07d69f5f494950747a9f03bf6cef039510
huxiaomei1 <1410067616@qq.com>
2021-12-05 13:12:51 +0800

    Initial commit

---
+++ src/main/java/cn/xcqd/dao/TestNewsDao.java
@@ -0,0 +1,13 @@
+package cn.xcqd.dao;
+
+import cn.xcqd.dao.impl.NewsDaoImpl;
+
+public class TestNewsDao {
+	public static void main(String[] args) {
+		NewsDao newsDao = new NewsDaoImpl();
+//		newsDao.add(22, 2, "测试数据22", "admin", "摘要", "内容测试", new Date());
+//		newsDao.update(22,"今天周五明天放假");
+//		newsDao.delete(22);
+		newsDao.getNewsList();
+	}
+}
